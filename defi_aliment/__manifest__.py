d1a92b24a8d8a323c43b8712ad9d242cd20e8c73
Mathieu Benoit <mathben@technolibre.ca>
2023-01-24 14:55:57 -0500

    [ADD] new module defi_aliment

---
+++ defi_aliment/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Defi Aliment",
+    "version": "12.0.1.0",
+    "author": "TechnoLibre",
+    "license": "AGPL-3",
+    "website": "https://technolibre.ca",
+    "application": True,
+    "depends": [
+        "code_generator",
+        "code_generator_hook",
+    ],
+    "installable": True,
+}
